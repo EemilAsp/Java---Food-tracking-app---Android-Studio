bdc18439eb4b7710bc9a299a2dc1c7434ca12aca
Eemil Aspholm <79214566+EemilAsp@users.noreply.github.com>
2021-03-21 14:47:03 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.datahaku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        foodDataHarvester fd = foodDataHarvester.getInstance();
+        String s = "o";
+        fd.readGSON(s);
+    }
+}
---
+++ userFoodDiary.java
@@ -0,0 +1,7 @@
+package com.example.datahaku;
+
+import java.util.Date;
+
+public class userFoodDiary {
+    public Date date;
+}
